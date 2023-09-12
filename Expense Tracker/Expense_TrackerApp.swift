d20ff416c429ff2d012afbfc381c8d60f9b34c8c
Clement Ojiguo <clementojigs@hotmail.com>
2023-09-12 09:41:36 +0100

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Clem OJ on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
